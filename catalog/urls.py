60e63e25dba3006f367d34dfeb088de66ded07b2
tghan <tayebe.ghanbari@gmail.com>
2018-09-21 23:53:07 +0430

    1397/06/30 2353

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name='catalog'
 
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.Index.as_view(), name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
 from . import models
+from django.views import generic
 
-def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
-    num_books=models.Book.objects.all().count()
-    num_instances=models.BookInstance.objects.all().count()
-    # Available books (status = 'a')
-    num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
-    num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
+class Index(generic.TemplateView):
+    template_name='catalog/index.html'
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(
-        request,
-        'catalog/index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_books'] = models.Book.objects.all().count()
+        context['num_instances'] = models.BookInstance.objects.all().count()
+        context['num_instances_available'] = models.BookInstance.objects.filter(status__exact='a').count()
+        context['num_authors'] = models.Author.objects.count()  # The 'all()' is implied by default.
+        return context
+
+class BookListView(generic.ListView):
+    template_name='catalog/book_list.html'
+
+    model=models.Book
+
+class BookDetailView(generic.DetailView):
+    template_name='catalog/book_detail.html'
+
+    model = models.Book
+# def index(request):
+#     """
+#     View function for home page of site.
+#     """
+#     # Generate counts of some of the main objects
+#     num_books=models.Book.objects.all().count()
+#     num_instances=models.BookInstance.objects.all().count()
+#     # Available books (status = 'a')
+#     num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
+#     num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
+#
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(
+#         request,
+#         'catalog/index.html',
+#         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+#     )
